c7a4c350d3e9103673f5d07c4be6fa0647ccb4fb
CristJimenez <123cristianjose@gmail.com>
2026-05-16 19:03:07 -0500

    UserManagementCli fixed

--- src/main/java/com/jcaa/usersmanagement/application/port/in/UpdateUserUseCase.java
+++ src/main/java/com/jcaa/usersmanagement/application/port/in/UpdateUserUseCase.java
@@ -1,9 +1,10 @@
 package com.jcaa.usersmanagement.application.port.in;
 
 import com.jcaa.usersmanagement.application.service.dto.command.UpdateUserCommand;
+import com.jcaa.usersmanagement.domain.model.UserModel;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
 public interface UpdateUserUseCase {
-  void execute(@NotNull @Valid UpdateUserCommand command);
+  UserModel execute(@NotNull @Valid UpdateUserCommand command);
 }
